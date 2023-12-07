9f2c408413963f2447b857299ac004af073bb2d0
DamianoL <damianolunardo@hotmail.it>
2023-12-07 15:25:29 +0100

    first commit

---
+++ resources/views/hompage.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <h2>{{ $title }} <h2>
+        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quidem placeat ad deserunt qui odit culpa impedit ea modi voluptatibus saepe, suscipit repudiandae distinctio consectetur iste nam, deleniti quasi velit.<p>
+    </div>
+</body>
+</html>

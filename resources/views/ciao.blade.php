b26b98128c30172eaabae203bc85f18d06f7bf97
DamianoL <damianolunardo@hotmail.it>
2023-12-07 16:35:03 +0100

    Fix

--- resources/views/ciao.blade.php
+++ resources/views/ciao.blade.php
@@ -1,12 +1,6 @@
-
-
 @extends('hompage')
-
-@section('title', 'Ciao')
-
 @section('content')
     <h1>Questa è una prova del menu</h1>
     <p>Tutto molto interessante, super prova, ancora prova</p>
-       
 @endsection
 
--- resources/views/questa.blade.php
+++ resources/views/questa.blade.php
@@ -1,9 +1,4 @@
-
-
 @extends('hompage')
-
-@section('title', 'Questa')
-
 @section('content')
     <h1> Questa è una prova del menu</h1>
     <p>Tutto molto interessante, grazie tutorial indiano di youtube</p>
